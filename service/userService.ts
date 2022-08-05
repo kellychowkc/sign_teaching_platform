24a8b5fe1655ffbcb5572ea01f7a8b57854538ef
Mickylui <mickyluihk@icloud.com>
2022-08-05 16:57:38 +0800

    checked: last features no problem

--- service/userService.ts
+++ service/userService.ts
@@ -8,7 +8,6 @@ export class UserService {
   constructor(private knex: Knex) {}
 
   async login(username: string): Promise<User[]> {
-    console.log("this is username form service:", username)
     const foundUser = await this.knex
       .select("*")
       .from("users")
@@ -21,25 +20,20 @@ export class UserService {
     let { username, password, first_name, last_name, email, phone_num} = body;
     const identity = "student";
     password = await hashPassword(password)
-    console.log("this is req input:",username, password, first_name, last_name, email, phone_num)
 
     // check if repeated
     const nameResult = await this.knex.select("*").from("users").where("username",username)
     const emailResult = await this.knex.select("*").from("users").where("email",email)
     const phoneNumResult = await this.knex.select("*").from("users").where("phone_num",phone_num)
 
-    console.log("this is checking:", nameResult,emailResult,phoneNumResult)
     if(nameResult.length == 0|| emailResult.length == 0 || phoneNumResult.length == 0){
       const createdUserId = await this.knex
       .insert({ username, password, first_name, last_name, email, phone_num, identity })
       .into("users")
       .returning("id");
-    console.log("inserting...", createdUserId)
     return createdUserId;
     }
-    console.log("empty array!")
-    return 
-  
+    return
   }
 
 
