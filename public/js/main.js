a51ab9f344e82a2e6c701251db5b37c53ad9529d
Mickylui <mickyluihk@icloud.com>
2022-08-17 12:57:07 +0800

    linked index html and finished admin css

--- public/js/main.js
+++ public/js/main.js
@@ -6,13 +6,14 @@ window.onload = () => {
     forceLogin();
 }
 
-
 function forceLogin() {
     document.querySelector(".face-time-teaching-btn").addEventListener("click", async() => {
         const result = await checkIfLogIn();
         console.log("this is checkIfLogIn:", result)
-        if (result === false) {
+        if (result == false) {
             window.location.href = "/html/login.html"
+        }else{
+            window.location.href="/html/userInfo.html"
         }
     })
 }
